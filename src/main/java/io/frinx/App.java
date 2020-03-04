aa1703c03424fd0d6f693735c5f333b4bc8304f2
Pavel Paroulek <pparoulek@frinx.io>
2020-03-04 13:06:30 +0100

    initial commit

---
+++ src/main/java/io/frinx/App.java
@@ -0,0 +1,13 @@
+package io.frinx;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
