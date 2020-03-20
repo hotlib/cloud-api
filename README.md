b73b7e9bd50f8bf0d2c0c910c53806de744ce462
Pavel Paroulek <pavel.paroulek@orange.com>
2020-03-20 09:09:20 +0100

    minor edits

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # cloud-api
+
+gRPC server, which receives data from a backend agent and stores them in a relational DB. 
--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
+
+# first create/update the database schema (if needed)
+java -cp ".:cloudapi-1.0-SNAPSHOT-jar-with-dependencies.jar" io.frinx.App -c
+
+# launch the grpc server
 java -cp ".:cloudapi-1.0-SNAPSHOT-jar-with-dependencies.jar" io.frinx.App
